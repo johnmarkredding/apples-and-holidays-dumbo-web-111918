eff4bd7df5e36833365b75459227835ce1e609bb
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 04:08:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,y|
     puts "#{season.capitalize}:"
     y.each do |holiday,b|
-      
+      holiday.to_s.split("_").each do |x|
       puts "  #{holiday.to_s.split("_").join(" ").capitalize}: #{b.join(", ")}"
     end
   end

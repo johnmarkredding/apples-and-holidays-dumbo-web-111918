16b863eda2136f177eff86bd1008aa00df600d46
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 04:10:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
       str = holiday.to_s.split("_").map! do |x|
         x.capitalize
       end.join(" ")
-      puts "  #{holiday.to_s.split("_").join(" ").capitalize}: #{b.join(", ")}"
+      puts "  #{str}: #{b.join(", ")}"
     end
   end
 end

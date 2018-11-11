2bed97bd4aa5c3a1b37ecbcf5f5bb8430a9e2306
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 04:16:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.select do |season, val|
-    val.select
+  holiday_hash.each do |season, val|
+    val.select do ||
+    end
   end
 end

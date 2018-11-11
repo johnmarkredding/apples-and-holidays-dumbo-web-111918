6506cc2acf6d04d962a63c05cda1329d01934741
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 03:53:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,8 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |x,y|
+    puts "#{x.capitalize}:"
     x.each do |a,b|
-      
     end
   end
 end
